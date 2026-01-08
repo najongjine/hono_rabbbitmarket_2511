5623b8b8fc183d5ee95b7805dab6acc70af27352
najongjine <najongjin3@hotmail.com>
2026-01-08 10:35:17 +0900

    .

---
+++ README.md
@@ -0,0 +1,19 @@
+```
+npm install
+npm run dev
+```
+
+npm i cross-env
+
+npm i dotenv
+
+npm i rimraf tsx
+
+package.json
+-scripts.dev 를
+
+cross-env NODE_ENV=development tsx watch src/index.ts
+
+```
+open http://localhost:3000
+```
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import * as dotenv from "dotenv";
+import { cors } from "hono/cors"; // <-- 보안 해재
+import { dbMiddleware } from "./db/db.js"; // DB
+
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
+dotenv.config({ path: envFile });
+
+const app = new Hono<HonoEnv>();
+app.use("*", dbMiddleware); // DB 등록
+app.use("*", cors()); // <-- 보안 해재
+/**
+ * 서버설정
+ * DB 설정
+ * 보안설정
+ */
+app.get("/", (c) => {
+  return c.text("health check!");
+});
+
+/** router 설정 */
+import hospitalRouter from "./router/hospital_router.js";
+import testRouter from "./router/test_router.js";
+import { HonoEnv } from "./types/types.js";
+app.route("/api/hospital", hospitalRouter);
+app.route("/api/test", testRouter);
+/** router 설정 END */
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
