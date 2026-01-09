f6008ab81f1bbf05638b1d7dc38b3824aa6d9b95
najongjine <najongjin3@hotmail.com>
2026-01-09 09:20:12 +0900

    .

--- src/types/types.ts
+++ src/types/types.ts
@@ -13,3 +13,32 @@ export type HonoEnv = {
   Bindings: Bindings;
   Variables: Variables;
 };
+
+export interface KakaoAddressResponse {
+  meta: {
+    total_count: number;
+  };
+  documents: {
+    road_address: {
+      address_name: string;
+      region_1depth_name: string;
+      region_2depth_name: string;
+      region_3depth_name: string;
+      road_name: string;
+      underground_yn: string;
+      main_building_no: string;
+      sub_building_no: string;
+      building_name: string;
+      zone_no: string;
+    } | null;
+    address: {
+      address_name: string;
+      region_1depth_name: string;
+      region_2depth_name: string;
+      region_3depth_name: string;
+      mountain_yn: string;
+      main_address_no: string;
+      sub_address_no: string;
+    } | null;
+  }[];
+}
